91a033ea99d04a3d652bddace0ef63f6001026b0
GooMonk <im.rahulpuro@gmail.com>
2023-01-22 13:07:39 +0530

    Improved Pow function and added benchmark for it

--- util/util.go
+++ util/util.go
@@ -1,5 +1,7 @@
 package util
 
+import "math"
+
 func SumN(n int64) int64 {
 	var res int64
 	if n%2 == 0 {
@@ -45,7 +47,7 @@ func Sieve(n int64) []bool {
 	return notPrime
 }
 
-func Pow(a, b int64) int64 {
+func LinPow(a, b int64) int64 {
 	var res int64 = 1
 	var i int64
 	for i = 0; i < b; i++ {
@@ -63,4 +65,56 @@ func IsPalindrome(n int64) bool {
 	}
 	return (nrev == n)
 
+}
+
+func Pow(a, b int64) int64 {
+	if b < 0 {
+		return math.MinInt64 // TODO : Support negative power as well
+	}
+	var res int64 = 1
+	if a == 0 || b == 0 {
+		return res
+	}
+	for b > 1 {
+		if b%2 != 0 {
+			res *= a
+		}
+		b /= 2
+		a *= a
+	}
+	return res * a
+}
+
+func RecPow(a, b int64) int64 {
+	if b < 0 {
+		return math.MinInt64 // TODO : Support negative power as well
+	}
+
+	if a == 0 || b == 0 {
+		return 1
+	}
+
+	if b%2 != 0 {
+		return RecPow(a*a, b/2) * a
+	}
+	return RecPow(a*a, b/2)
+}
+
+func BinarySearch(arr []int64, ele int64) int64 {
+	i := 0
+	j := len(arr)
+	if j == 0 {
+		return -1
+	}
+	for i < j {
+		mid := i + (j-i)/2
+		if arr[mid] == ele {
+			return int64(mid)
+		} else if arr[mid] < ele {
+			i = mid + 1
+		} else {
+			j = mid
+		}
+	}
+	return -1
 }
--- util/util_test.go
+++ util/util_test.go
@@ -25,3 +25,30 @@ func subBenchmarkGCD(a, b int64, bench *testing.B) {
 		Gcd(a, b)
 	}
 }
+
+func BenchmarkPow(b *testing.B) {
+	input := [][]int64{{2, 11}, {47, 3}, {631, 8}, {9, 2}}
+	for i := 0; i < len(input); i++ {
+		b.Run(fmt.Sprintf("Pow: For Input: %d , %d : Out: %d", input[i][0], input[i][1], Pow(input[i][0], input[i][1])), func(b *testing.B) { subBenchmarkPow(input[i][0], input[i][1], b) })
+		b.Run(fmt.Sprintf("RecPow: For Input: %d , %d : Out: %d", input[i][0], input[i][1], RecPow(input[i][0], input[i][1])), func(b *testing.B) { subBenchmarkRecPow(input[i][0], input[i][1], b) })
+		b.Run(fmt.Sprintf("LinPow: For Input: %d , %d: Out: %d", input[i][0], input[i][1], LinPow(input[i][0], input[i][1])), func(b *testing.B) { subBenchmarkLinPow(input[i][0], input[i][1], b) })
+	}
+}
+
+func subBenchmarkPow(a, b int64, bench *testing.B) {
+	for i := 0; i <= bench.N; i++ {
+		Pow(a, b)
+	}
+}
+
+func subBenchmarkLinPow(a, b int64, bench *testing.B) {
+	for i := 0; i <= bench.N; i++ {
+		LinPow(a, b)
+	}
+}
+
+func subBenchmarkRecPow(a, b int64, bench *testing.B) {
+	for i := 0; i <= bench.N; i++ {
+		RecPow(a, b)
+	}
+}
